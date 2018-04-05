b0dbe0afab8215fa7ec72cd32a509bb6c9cc1ac2
Robert Wagner <rwwagner90@gmail.com>
2018-04-05 12:29:19 -0400

    Copy treeForPublic, add rss package

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 'use strict';
 
+const RSS = require('rss');
+
 module.exports = {
-  name: 'prember-rss-feed'
+  name: 'prember-rss-feed',
+
+  // TODO replace this with a real treeForPublic. This was just copied for reference from prember-sitemap-generator
+  treeForPublic() {
+    this._super.treeForPublic && this._super.treeForPublic.apply(this, arguments);
+
+    if (process.env.EMBER_ENV === 'production') {
+      const premberOptions = this.app.options['prember'];
+      const baseRoot = premberOptions.baseRoot;
+      if (!baseRoot) {
+        return this.ui.writeLine(chalk.red(
+          'ERROR: You must define `baseRoot` for prember-sitemap-generator to generate sitemaps.'
+        ));
+      }
+      const urls = premberOptions && premberOptions.urls ? premberOptions.urls : [];
+      const tree = writeFile('/prember-sitemap-generator/sitemap.xml', generateSitemap(baseRoot, urls));
+
+      this.ui.writeLine(chalk.green('sitemap.xml successfully created!'));
+
+      return new Funnel(tree, {
+        srcDir: 'prember-sitemap-generator'
+      });
+    }
+  }
 };
