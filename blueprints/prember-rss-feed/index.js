8144faca1b7ed6ea0457c01522f53a18ff12009b
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-04-13 22:31:08 +0530

    Fixed default blueprint & added html descriptions in feeds

--- blueprints/prember-rss-feed/index.js
+++ blueprints/prember-rss-feed/index.js
@@ -1,15 +1,21 @@
 /* eslint-env node */
+
+let chalk = require('chalk');
+let { green }  = chalk;
+let output = `
+Generated a feed config. Configure it and then use it in your ember-cli-build.js as shown below,
+
+let app = new EmberAddon(defaults, {
+  rssFeed: require('./config/rss-feed')
+});
+`
+
 module.exports = {
-  description: ''
+  description: 'Generates rss feed config',
 
-  // locals(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
+  normalizeEntityName() {},
 
-  // afterInstall(options) {
-  //   // Perform extra work here.
-  // }
+  afterInstall() {
+    this.ui.write(green(output));
+  }
 };
